2a180d01045fa19da2d3a5c33f748ab0b273d3c8
Ron Desmarais <rd@uvic.ca>
2015-08-02 22:04:31 +0000

    echo term

---
+++ c64echoterm.py
@@ -0,0 +1,49 @@
+import time
+import serial
+import binascii
+
+# configure the serial connections (the parameters differs on the device you are connecting to)
+ser = serial.Serial(
+    port='/dev/ttyUSB0',
+    baudrate=1200,
+    parity=serial.PARITY_ODD,
+    stopbits=serial.STOPBITS_ONE,
+    bytesize=serial.SEVENBITS
+)
+try:
+    ser.open()
+    print "Try is Open"
+    ser.isOpen()
+except Exception as e:
+    #ser.close()
+    print "Opne serial error %s"%e
+
+print 'Enter your commands below.\r\nInsert "exit" to leave the application.'
+
+input=1
+while 1 :
+    # get keyboard input
+    input = raw_input(">> ")
+        # Python 3 users
+        # input = input(">> ")
+    if input == 'exit':
+        ser.close()
+        exit()
+    else:
+        # send the character to the device
+        # (note that I happend a \r\n carriage return and line feed to the characters - this is requested by my device)
+        ser.write(input+'\n')
+        out = ''
+        # let's wait one second before reading output (let's give device time to answer)
+        time.sleep(1)
+        while ser.inWaiting() > 0:
+            #out += binascii.unhexlify('%x'%ser.read())
+            b=ser.read()
+            #print "%s versus %s"%(b,ord(b))
+            out += b
+        if out != '':
+            print out
+            
+
+
+
